e04a6a07789f28e293599babc952432ca2c3f0a8
Tomoaki Yagishita <tyagishi@gmail.com>
2020-06-18 11:44:20 +0900

    make attribute public

--- Sources/SPMForApp/LibraryModel.swift
+++ Sources/SPMForApp/LibraryModel.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public struct LibraryModel {
-    let version = 2.0
-    let data = "this is data from Library"
+    public let version = 2.0
+    public let data = "this is data from Library"
     
     public init() {
         // do nothing
