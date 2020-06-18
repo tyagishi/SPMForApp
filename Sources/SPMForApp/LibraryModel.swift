c340402cc11cde6496c7e2c2ca9856234ae8166f
Tomoaki Yagishita <tyagishi@gmail.com>
2020-06-18 11:40:18 +0900

    add sample

---
+++ Sources/SPMForApp/LibraryModel.swift
@@ -0,0 +1,13 @@
+//
+//  LibraryModel.swift
+//  
+//
+//  Created by Tomoaki Yagishita on 2020/06/18.
+//
+
+import Foundation
+
+struct LibraryModel {
+    let version = 2.0
+    let data = "this is data from Library"
+}
